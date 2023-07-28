861c23a5baf5ead0e98ee578b354d4098e26b38d
Yasmine MAATAOUI <yasminemaataoui12@gmail.com>
2023-07-28 10:09:52 +0100

    update_Settings

--- Projet_GQ/settings.py
+++ Projet_GQ/settings.py
@@ -129,6 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_USE_TLS = True  
 EMAIL_HOST = 'smtp.gmail.com'  
-EMAIL_HOST_USER = '#'  
-EMAIL_HOST_PASSWORD = '#'  
-EMAIL_PORT = 587  
+EMAIL_HOST_USER = 'maildetesting1@gmail.com'  
+EMAIL_HOST_PASSWORD = 'fotxiknccabjevmd'  
+EMAIL_PORT = 587
