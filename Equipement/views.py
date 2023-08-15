cd51ec736a4cafb0886e667202dc78e7e1494008
Yasmine MAATAOUI <yasminemaataoui12@gmail.com>
2023-08-15 10:50:19 +0100

    final commit

--- Equipement/views.py
+++ Equipement/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
+import codecs
 import csv
-import os
+import io
 from Equipement.filters import EquipementFiltre
 from django.contrib import messages
 from django.shortcuts import render, redirect
@@ -77,63 +77,62 @@ def import_csv(request):
             return render(request, 'Equipement/import_csv.html')
 
         try:
-            decoded_file = csv_file.read().decode('iso-8859-1')  # Utilisez l'encodage ISO-8859-1 (latin-1) pour lire le fichier CSV
-            csv_data = csv.reader(decoded_file.splitlines(), delimiter=',')
+           
+          with csv_file.open('rb') as f:
+            decoded_file = f.read().decode('utf-8')
+            csv_data = csv.reader(io.StringIO(decoded_file), delimiter=',')
+                # Ignore the first line (header)
+            next(csv_data)
 
             for row in csv_data:
-                # Vérifiez que toutes les données nécessaires sont présentes dans la ligne du CSV
-                if len(row) >= 13:
-                    # Récupérez les données de chaque colonne
-                    reference = row[0]
-                    etat = row[1]
-                    marque = row[2]
-                    date_acquisition = row[3]
-                    laboratoire_name = row[4]
-                    localisation = row[5]
-                    telephone = row[6]
-                    email = row[7]
-                    abrv = row[8]
-                    etablissement_name = row[9]
-                    adresse = row[10]
-                    etablissement_telephone = row[11]
-                    etablissement_email = row[12]
+            # Récupérez les données de chaque colonne
+                reference = row[0]
+                etat = 'Installé' 
+                marque = row[2]
+                date_acquisition = row[3]
+                laboratoire_name = row[4]
+                localisation = row[5]
+                telephone = row[6]
+                email = row[7]
+                abrv = row[8]
+                etablissement_name = row[9]
+                adresse = row[10]
+                etablissement_telephone = row[11]
+                etablissement_email = row[12]
+                categorie = row[13]
 
-                    # Recherchez l'établissement correspondant dans la base de données en utilisant le nom de l'établissement
-                    try:
-                        etablissement = Etablissement.objects.get(Nom_Etab=etablissement_name)
-                    except Etablissement.DoesNotExist:
-                        # Si l'établissement n'existe pas, créez-le et enregistrez-le dans la base de données
-                        etablissement = Etablissement.objects.create(
-                            Nom_Etab=etablissement_name,
-                            Adresse=adresse,
-                            telephone=etablissement_telephone,
-                            email=etablissement_email
-                        )
-                    
-                    # Vérifiez si le laboratoire existe déjà dans la base de données
-                    laboratoire_exists = Laboratoire.objects.filter(Nom_Lab=laboratoire_name).exists()
-                    if laboratoire_exists:
-                        messages.warning(request, f"Le laboratoire '{laboratoire_name}' existe déjà dans la base de données. Le laboratoire et l'équipement ne seront pas importés.")
-                        continue
+            
+                etablissement, created_etab = Etablissement.objects.get_or_create(
+                    Nom_Etab=etablissement_name,
+                    defaults={
+                        'Adresse': adresse,
+                        'telephone': etablissement_telephone,
+                        'email': etablissement_email
+                    }
+                )
+                
+                # Recherchez le laboratoire correspondant dans la base de données
+                laboratoire, created_lab = Laboratoire.objects.get_or_create(
+                    Nom_Lab=laboratoire_name,
+                    defaults={
+                        'Localisation': localisation,
+                        'Telephone': telephone,
+                        'Email': email,
+                        'Abrv': abrv,
+                        'Etablissement': etablissement
+                    }
+                )
+                
+                # Créez une instance d'Equipement et enregistrez-la dans la base de données
+                Equipement.objects.create(
+                    Reference=reference,
+                    Etat=etat,
+                    Date_Acquisition=date_acquisition,
+                    Marque=marque,
+                    Laboratoire=laboratoire,
+                    Categorie=categorie
+                )
 
-                    # Créez une instance de Laboratoire et enregistrez-la dans la base de données
-                    laboratoire = Laboratoire.objects.create(
-                        Nom_Lab=laboratoire_name,
-                        Localisation=localisation,
-                        Telephone=telephone,
-                        Email=email,
-                        Abrv=abrv,
-                        Etablissement=etablissement
-                    )
-
-                    # Créez une instance d'Equipement et enregistrez-la dans la base de données
-                    Equipement.objects.create(
-                        Reference=reference,
-                        Etat=etat,
-                        Date_Acquisition=date_acquisition,
-                        Marque=marque,
-                        Laboratoire=laboratoire
-                    )
         
             # Redirigez ou renvoyez une réponse appropriée en fonction de votre besoin
             messages.success(request, 'Importation CSV réussie.')
